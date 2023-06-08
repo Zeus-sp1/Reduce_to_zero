f12b23ecdd48137bf4cc706144c0eda8201938df
Sunday Peter <sundaypetersp12@gmail.com>
2023-06-08 23:52:23 +0100

    Add files via upload

---
+++ Reduce.java
@@ -0,0 +1,11 @@
+
+class Reduce{
+    public num(int n){
+
+           
+        for (i = 0; i < n; i++){
+            int evenNumber = i % 2 == 0;
+            int oddNumber = valueOfIndex(i) - 1;
+        }
+    }
+}
